8a9e341a5ce6ce18ee4dff1913e501bcd3f8ba6e
Jamie Jordan <jordan.jamie45@gmail.com>
2023-05-03 23:34:02 +0100

    csharpcore: invert if statements to reduce nesting

--- csharpcore/GildedRoseKata/GildedRose.cs
+++ csharpcore/GildedRoseKata/GildedRose.cs
@@ -64,31 +64,27 @@ public class GildedRose
                 item.SellIn -= 1;
             }
 
-            if (item.SellIn < 0)
+            if (item.SellIn >= 0) continue;
+            if (item.Name != "Aged Brie")
             {
-                if (item.Name != "Aged Brie")
+                if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                    if (item.Quality <= 0) continue;
+                    if (item.Name != "Sulfuras, Hand of Ragnaros")
                     {
-                        if (item.Quality > 0)
-                        {
-                            if (item.Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                item.Quality -= 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        item.Quality -= item.Quality;
+                        item.Quality -= 1;
                     }
                 }
                 else
                 {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality += 1;
-                    }
+                    item.Quality -= item.Quality;
+                }
+            }
+            else
+            {
+                if (item.Quality < 50)
+                {
+                    item.Quality += 1;
                 }
             }
         }
